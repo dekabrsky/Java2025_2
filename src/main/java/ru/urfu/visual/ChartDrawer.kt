7fa14f409ff22105819c5fad8e9535eafe6bea96
dryddot-lang <dryddot@gmail.com>
2025-10-10 09:24:20 +0500

    nothing changes

--- src/main/java/ru/urfu/visual/ChartDrawer.kt
+++ src/main/java/ru/urfu/visual/ChartDrawer.kt
@@ -10,7 +10,7 @@ import ru.urfu.model.Player
 import javax.swing.JFrame
 
 object ChartDrawer {
-    fun GoalsVsValue(players: List<Player>) {
+    fun asdasdGoalsVsValue(players: List<Player>) {
         val series = XYSeries("FORWARD")
 
         players.filter { it.position.equals("FORWARD", ignoreCase = true) }
